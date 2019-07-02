6ebc43b68f96844a62535c069c92027066df0c27
maciek <maciekjablonsky@gmail.com>
2019-07-02 23:20:21 +0200

    ADD script which builds all tests and prints how many of them succeed

---
+++ singly_linked_list/tests/run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INIT="$(pwd)"
+mkdir -p build
+cd build
+cmake ..
+mkdir -p bin
+make
+mv test* bin
+
+PASSED=0
+ALL=0
+FAILED_TESTS=""
+cd "$INIT"
+for FILE in build/bin/* ; do
+    ./"$FILE"
+    if (( $? == 0 ))  ; then
+        PASSED=$((PASSED + 1))
+    else
+        FAILED_TESTS="${FAILED_TESTS}$(echo ${FILE} | sed 's#.*/##g')\n"
+    fi
+    ALL=$((ALL + 1))
+done
+
+printf "\n$PASSED/$ALL tests passed.\n"
+if [[ -n ${FAILED_TESTS} ]]; then
+    echo "Failed tests:"
+    printf "${FAILED_TESTS}"
+fi
