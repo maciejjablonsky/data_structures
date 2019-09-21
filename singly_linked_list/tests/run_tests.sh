90ac625d93c6b84b0955b58a2e12e34638ab9316
maciek <maciekjablonsky@gmail.com>
2019-09-21 15:20:08 +0200

    Pretty printing

--- singly_linked_list/tests/run_tests.sh
+++ singly_linked_list/tests/run_tests.sh
@@ -10,6 +10,14 @@ make
 
 mv test_* bin
 
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
 PASSED=0
 ALL=0
 FAILED_TESTS=""
@@ -19,13 +27,21 @@ for FILE in build/bin/* ; do
     if (( $? == 0 ))  ; then
         PASSED=$((PASSED + 1))
     else
-        FAILED_TESTS="${FAILED_TESTS}$(echo ${FILE} | sed 's#.*/##g')\n"
+        FAILED_TESTS="${FAILED_TESTS} ${red}- $(echo ${FILE} | sed 's#.*/##g')${end}\n"
     fi
     ALL=$((ALL + 1))
 done
 
-printf "\n$PASSED/$ALL tests passed.\n"
+printf "\n${mag}===========================================${end}\n"
+if [[ "$PASSED" == "$ALL" ]]; then
+printf "${grn}"
+else
+printf "${cyn}"
+fi
+printf "\n$PASSED/$ALL tests passed.\n${end}"
 if [[ -n ${FAILED_TESTS} ]]; then
-    echo "Failed tests:"
+    printf "${red}\nFailed tests:\n${end}"
     printf "${FAILED_TESTS}"
+else
+    printf "\n${grn}All tests PASSED${end}.\n"
 fi
