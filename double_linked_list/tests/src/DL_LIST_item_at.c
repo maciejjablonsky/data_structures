7812e1c3a6cb0c8b3a8c0c0ae027e921ad060ff9
maciek <maciekjablonsky@gmail.com>
2019-10-20 17:10:48 +0200

    added DL_LIST_item_at() tests

---
+++ double_linked_list/tests/src/DL_LIST_item_at.c
@@ -0,0 +1,75 @@
+//
+// Created by maciek on 20.10.19.
+//
+
+#include <setjmp.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <cmocka.h>
+#include "../../src/dl_list.h"
+#include <assert.h>
+
+dl_list_t *list = NULL;
+#define ITEMS_COUNT 5
+
+static int setup(void **state)
+{
+    list = DL_LIST_create(sizeof(int), DL_COPY_ITEM, NULL);
+    for (int i = 0; i < ITEMS_COUNT; ++i)
+    {
+        DL_LIST_add_item(list, &i);
+    }
+    return 0;
+}
+
+static void DL_LIST_item_at__get_1st_item(void **state)
+{
+    (void) state;
+
+    int *item = DL_LIST_item_at(list, 0);
+    assert_non_null(item);
+    assert_int_equal(*item, 0);
+}
+
+static void DL_LIST_item_at__get_3rd_item(void **state)
+{
+    (void) state;
+    assert(ITEMS_COUNT >= 2);
+    int *item = DL_LIST_item_at(list, 2);
+    assert_non_null(item);
+    assert_int_equal(*item, 2);
+}
+
+static void DL_LIST_item_at__get_last_item(void **state)
+{
+    (void) state;
+    int *item = DL_LIST_item_at(list, ITEMS_COUNT - 1);
+    assert_non_null(item);
+    assert_int_equal(*item, ITEMS_COUNT - 1);
+}
+
+static void DL_LIST_item_at__negative_index(void **state)
+{
+    (void) state;
+    int *item = DL_LIST_item_at(list, -2);
+    assert_null(item);
+}
+
+static void DL_LIST_item_at__too_big_index(void **state)
+{
+    (void) state;
+    int *item = DL_LIST_item_at(list, ITEMS_COUNT);
+    assert_null(item);
+}
+
+int main(void)
+{
+    struct CMUnitTest tests[] = {cmocka_unit_test(DL_LIST_item_at__get_1st_item),
+                                 cmocka_unit_test(DL_LIST_item_at__get_3rd_item),
+                                 cmocka_unit_test(DL_LIST_item_at__get_last_item),
+                                 cmocka_unit_test(DL_LIST_item_at__negative_index),
+                                 cmocka_unit_test(DL_LIST_item_at__too_big_index),
+
+    };
+    return cmocka_run_group_tests(tests, setup, NULL);
+}
